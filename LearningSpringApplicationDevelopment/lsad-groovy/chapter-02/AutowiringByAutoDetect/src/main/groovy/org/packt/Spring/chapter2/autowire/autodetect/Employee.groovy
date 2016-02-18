f2f661fd7e7654b9a2368ad5285e8486e850ba5e
kingsmiler <kingsmiler@163.com>
2016-02-18 17:48:26 +0800

    add lsad AutowiringByAutoDetect

---
+++ LearningSpringApplicationDevelopment/lsad-groovy/chapter-02/AutowiringByAutoDetect/src/main/groovy/org/packt/Spring/chapter2/autowire/autodetect/Employee.groovy
@@ -0,0 +1,10 @@
+package org.packt.Spring.chapter2.autowire.autodetect
+
+import groovy.transform.CompileStatic
+import groovy.transform.ToString
+
+@CompileStatic
+@ToString
+class Employee {
+    int employeeId
+}
---
+++ LearningSpringApplicationDevelopment/lsad-groovy/chapter-02/AutowiringByAutoDetect/src/main/java/org/packt/Spring/chapter2/autowire/autodetect/EmployeeService.java
@@ -0,0 +1,15 @@
+package org.packt.Spring.chapter2.autowire.autodetect;
+
+public class EmployeeService {
+
+	private Employee employee;
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public EmployeeService(Employee employee) {
+		this.employee = employee;
+	}
+
+}
