9512a2c424e931e6928406fe1ea44d73d8cbef4e
kingsmiler <kingsmiler@163.com>
2016-02-20 14:36:54 +0800

    update groovy main, and its constructor

--- LearningSpringApplicationDevelopment/lsad-groovy/chapter-02/AutowiringByType/src/main/groovy/org/packt/Spring/chapter2/autowirebytype/PayrollSystem.groovy
+++ LearningSpringApplicationDevelopment/lsad-groovy/chapter-02/AutowiringByType/src/main/groovy/org/packt/Spring/chapter2/autowirebytype/PayrollSystem.groovy
@@ -1,15 +1,14 @@
 package org.packt.Spring.chapter2.autowirebytype
 
-import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
 
 class PayrollSystem {
-    static void main(String[] args) {
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("beans.xml");
 
-        EmployeeService employeeService = context.getBean("employeeServiceBean");
+    static main(args) {
+        ClassPathXmlApplicationContext context = ["beans.xml"]
+        EmployeeService service = context.getBean("employeeServiceBean");
 
-        println(employeeService.getEmployee());
+        println(service.getEmployee());
     }
+
 }
